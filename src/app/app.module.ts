38388ae413343cf01c79771e12cf12529d554e2c
Michel Lutegar <mlutegar@gmail.com>
2023-03-19 18:08:58 -0300

    feat: criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ExibicaoComponent } from './exibicao/exibicao.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExibicaoComponent,
+    FormularioComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
