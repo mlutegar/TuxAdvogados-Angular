90eb2bccde697a14659a78d5186bb9eea945ed4a
Michel Lutegar <mlutegar@gmail.com>
2023-03-20 18:58:33 -0300

    feat: adicionando o home

---
+++ src/app/page/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  descricao = "A TuxAdvogados é uma empresa do ramo jurídico, que está sempre buscando melhorar o atendimento aos seus clientes. Pensando nisso, a empresa contratou nossos serviços para desenvolver uma aplicação para cadastro de clientes."
+
+}
