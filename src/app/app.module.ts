0337bacf3cf828fde6e98ebef90bc0a0bab68a62
Michel Lutegar <mlutegar@gmail.com>
2023-03-19 23:18:57 -0300

    feat: configurando as rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,13 @@ import { HomeComponent } from './home/home.component';
 import { ExibicaoComponent } from './exibicao/exibicao.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { SobreComponent } from './sobre/sobre.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'exibicao', component: ExibicaoComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'sobre', component: SobreComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +26,8 @@ import { SobreComponent } from './sobre/sobre.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
