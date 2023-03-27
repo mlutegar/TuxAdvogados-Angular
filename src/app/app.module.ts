728acab139e95af89c4c4b178131e2c28e27fb26
Michel Lutegar <mlutegar@gmail.com>
2023-03-26 22:33:57 -0300

    feat: integrando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {AngularFireModule} from '@angular/fire/compat'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { SobreComponent } from './page/sobre/sobre.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { ClientesComponent } from './page/clientes/clientes.component';
+import { environment } from 'src/environments/environments.prod';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { ClientesComponent } from './page/clientes/clientes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase) // O objetivo desse import é fazer a ligação com o firebase
   ],
   providers: [],
   bootstrap: [AppComponent]
