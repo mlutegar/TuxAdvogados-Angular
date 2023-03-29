c532ffc027062a35d5c329d7e8912b0f4a9c2a84
Michel Lutegar <mlutegar@gmail.com>
2023-03-29 18:05:55 -0300

    feat: adicionando o firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import {AngularFireModule} from '@angular/fire/compat'
+import { FormsModule} from '@angular/forms';
+
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './page/home/home.component';
 import { ExibicaoComponent } from './component/exibicao/exibicao.component';
 import { FormularioComponent } from './page/formulario/formulario.component';
@@ -12,7 +15,7 @@ import { SobreComponent } from './page/sobre/sobre.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { ClientesComponent } from './page/clientes/clientes.component';
-import { environment } from 'src/environments/environments.prod';
+import { environment } from 'src/environments/environments';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,10 @@ import { environment } from 'src/environments/environments.prod';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    AngularFireModule.initializeApp(environment.firebase) // O objetivo desse import é fazer a ligação com o firebase
+    AngularFireModule.initializeApp(environment.firebase), // O objetivo desse import é fazer a ligação com o firebase
+
+    // Agora vou importar o AngularFirestorModule, que é responsavel por fornecer as ferramentas dos banco de dados
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-    production: false,
-    firebase: {
-        apiKey: "AIzaSyBIqXdyhuUes0L0Z4ODBiMGz-oEwmiIDhU",
-        authDomain: "tuxadvogados-bb4db.firebaseapp.com",
-        projectId: "tuxadvogados-bb4db",
-        storageBucket: "tuxadvogados-bb4db.appspot.com",
-        messagingSenderId: "131348266628",
-        appId: "1:131348266628:web:aa4cf759abb8325035ccd1"
-    }
-};
+    production: true
+  };
